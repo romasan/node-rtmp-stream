183591d4e42323046c02af6cf20890df682006d7
Roman Bauer <r.bauer@vk.team>
2023-11-22 02:50:56 +0300

    prepare db files

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd ../
+mkdir db
+mkdir db/sessions
 echo -n > db/pixels.log
 npm run tools recover ./db/pixels.log NOIMAGE ./db/inout.png 112 112
 echo "0;0;112;112;0;0" > db/expands.log
