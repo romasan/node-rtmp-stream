136d2fffdae2379f8621c502fd5c5a7ef352b9b3
Roman Bauer <r.bauer@vk.team>
2024-12-30 02:48:36 +0300

    fix new ep script

--- scripts/newep.sh
+++ scripts/newep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+cp assets/s3e1.png db/inout.png
 cd ./db
 echo -n > pixels.log
 echo "0;0;112;112;0;0" > expands.log
 # npm run tools recover ./db/pixels.log NOIMAGE ./db/inout.png 112 112
-cp assets/s3e1.png db/inout.png
