f2eadb9bdacc85478e25f7442f8bf8470c14740e
Roman Bauer <r.bauer@vk.team>
2024-12-30 02:46:29 +0300

    fix stream

--- server/utils/stream.ts
+++ server/utils/stream.ts
@@ -7,7 +7,7 @@ import fs from 'fs';
 import { Log } from './log';
 import { getImageBuffer } from './canvas';
 
-const { stream: { rtmpHostKey, inputAudio, FFMPEGLog, streamFile, streamFilePath, interval } } = require('../config.json');
+const { stream: { rtmpHostKey, inputAudio, FFMPEGLog, streamFile, interval } } = require('../config.json');
 
 const args = `-thread_queue_size 1024 -i ${inputAudio} -f image2pipe -framerate 2 -i - -f flv -vcodec libx264 -pix_fmt yuv420p -preset slow -r 25 -g 30 -movflags +faststart ${rtmpHostKey}`;
 const ffmpeg = streamFile ? null : spawn('ffmpeg', args.split(' '));
@@ -32,14 +32,8 @@ const writeCanvas = () => {
 
 		if (buf) {
 			onTransaction = true;
-			// fs.writeFileSync(streamFile, buf);
-			const now = Date.now();
-			fs.writeFile(streamFilePath + 'output.tmp', buf, () => {
-			// fs.writeFile(streamFilePath + 'output.tmp', buf, () => {
+			fs.writeFile(streamFile, buf, () => {
 				onTransaction = false;
-				// fs.rename(streamFile + '.tmp', streamFile, () => {});
-				fs.rename(streamFilePath + 'output.tmp', streamFilePath + index + '.png', () => {});
-				// fs.rename(streamFilePath + 'output.tmp', streamFilePath + '000' + index + '.png', () => {});
 			});
 		}
 	} else {
