0dc508702f8321a7a8f2c31bf55a0257db96e2bf
Roman Bauer <r.bauer@vk.team>
2026-01-05 19:28:16 +0300

    ref: debug

--- server/tools/debugServer.js
+++ server/tools/debugServer.js
@@ -3,6 +3,9 @@ const http = require('http');
 const callback = (req, res) => {
     console.log('==== origin:', req.headers['origin']);
     console.log('==== url:', req.url);
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('=)');
 };
 
 const debugServer = (port) => {
